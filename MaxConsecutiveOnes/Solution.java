8eef303e0fcd2c7e0752719de644174bc6ed3da9
p0znyaks <p0zny4ks@yandex.ru>
2024-12-01 20:08:09 +0300

    solution optimized

--- MaxConsecutiveOnes/Solution.java
+++ MaxConsecutiveOnes/Solution.java
@@ -1,6 +1,28 @@
 package Leetcode.MaxConsecutiveOnes;
 
-// runtime 3ms
+// 1 variant of solution
+// runtime 0ms
+// time complexity O(N)
+
+//class Solution {
+//    public int findMaxConsecutiveOnes(int[] nums) {
+//        int max = 0;
+//        int current = 0;
+//
+//        for(int i : nums) {
+//            if (i == 1) {
+//                current++;
+//            } else {
+//                current = 0;
+//            }
+//            max = Math.max(max, current);
+//        }
+//        return max;
+//    }
+//}
+
+// 2 variant of solution
+// runtime 0ms
 // time complexity O(N)
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
@@ -8,11 +30,7 @@ class Solution {
         int current = 0;
 
         for(int i : nums) {
-            if (i == 1) {
-                current++;
-            } else {
-                current = 0;
-            }
+            current = current * i + i; // т.к. массив двоичный использую такой метод подсчёта
             max = Math.max(max, current);
         }
         return max;
