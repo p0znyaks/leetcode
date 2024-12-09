d5a2a9dc0f1db6769048e9b745d5c5dd333d9bd2
p0znyaks <p0zny4ks@yandex.ru>
2024-12-09 16:57:40 +0300

    solution added

---
+++ BuildArrayFromPermutation/Solution.java
@@ -0,0 +1,15 @@
+package Leetcode.BuildArrayFromPermutation;
+
+// runtime 0ms
+// time complexity O(N)
+public class Solution {
+    public int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[nums[i]];
+        }
+
+        return ans;
+    }
+}
