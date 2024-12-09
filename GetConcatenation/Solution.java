359cf98073b57a39e8a443338c653f04ac8acddb
p0znyaks <p0zny4ks@yandex.ru>
2024-12-09 16:47:31 +0300

    solution added

---
+++ GetConcatenation/Solution.java
@@ -0,0 +1,17 @@
+package Leetcode.GetConcatenation;
+
+// runtime 0ms
+// time complexity O(N)
+public class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n * 2];
+
+        for(int i = 0; i < nums.length; i++) {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+
+        return ans;
+    }
+}
